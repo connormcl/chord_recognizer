0b4086b25e737ffad34fd0e341dedc9f3bb31474
connormcl <connor.mclaughlin@yale.edu>
2018-05-05 16:29:25 -0400

    new config file

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
-chords = ['A', 'Am', 'Bm', 'C', 'D', 'Dm', 'E', 'Em', 'F', 'G', 'A7', 'B', 'C7', 'E7', 'G7', 'D7', 'Cm', 'Fm', 'Gm']
+# config.py – Connor McLaughlin
+
+import os
+
+root_dir = 'chords' # parent directory for all chord directories
+if os.path.isdir(root_dir): # if WAV files exist, dynamically load chords
+	chords = [x for x in os.listdir(root_dir) if not os.path.isfile(x)] # all chord directories
+	chords = [x[0].upper() + x[1:] for x in chords] # formatting
+	chord_nums = [len(os.listdir(root_dir+'/'+chord)) for chord in chords] # number of wav files for each chord
+else: # WAV files don't exist, so presume default (note: cannot generate PCP vectors in this case)
+	chords = ['A', 'Am', 'Bm', 'C', 'D', 'Dm', 'E', 'Em', 'F', 'G', 'A7', 'B', 'C7', 'E7', 'G7', 'D7', 'Cm', 'Fm', 'Gm']
+	chords = sorted(chords)
 semi_tones = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b']
-dir_names = [x.lower() for x in chords] # list of directory names for chords
-connor_nums = [200, 200, 100, 100, 100, 100, 50, 50, 50]
