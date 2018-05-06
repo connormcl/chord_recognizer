f1be715206e0ddc32935a05f9096e32d6786f2e6
connormcl <connor.mclaughlin@yale.edu>
2018-05-05 22:50:30 -0400

    Plots learning curve after training model

--- train_model.py
+++ train_model.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 import time
 from network import Network
+import matplotlib.pyplot as plt
 
 def main():
 	# Load training and eval data
@@ -16,8 +17,13 @@ def main():
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 	print('done.')
 
-	net = Network(model_dest='checkpoints/model0.ckpt')
-	net.train(X_train, y_train, X_test, y_test, num_epochs=120000, batch_size=200)
+	net = Network(model_dest='checkpoints/model0.ckpt', l1_reg=0.0005, lr=0.0001)
+	_, _, losses = net.train(X_train, y_train, X_test, y_test, num_epochs=100000, batch_size=300, restore=True)
+	plt.plot(losses)
+	plt.xlabel('Epoch')
+	plt.ylabel('Loss')
+	plt.title('Neural Network Learning Curve')
+	plt.show()
 
 if __name__ == '__main__':
 	main()
